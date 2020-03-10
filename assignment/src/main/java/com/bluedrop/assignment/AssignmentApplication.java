4efe12359cc9936f530ab885b4d37d1c3fb26d89
Ante Burilovic <ante.burilovic.dev@gmail.com>
2020-03-10 09:24:36 +0100

    set default time zone to UTC

--- assignment/src/main/java/com/bluedrop/assignment/AssignmentApplication.java
+++ assignment/src/main/java/com/bluedrop/assignment/AssignmentApplication.java
@@ -3,11 +3,19 @@ package com.bluedrop.assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AssignmentApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
 
 }
